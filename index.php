7d5dde73f1a1d09e688a2137b9a1a8a8592474c9
AllanFelipeReis <allanreis3006@gmail.com>
2020-09-17 00:19:04 -0300

    Update

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ include 'functions.php';
 <div class="content">
 	<h2>Home</h2>
 	<p>CRUD em PHP!</p>
+	<p>Projeto utilizado para simular um CRUD de um table people</p>
+	<p>https://github.com/AllanFelipeReis/CRUD-PHP.git</p>
 </div>
 
 <?=template_footer()?>
